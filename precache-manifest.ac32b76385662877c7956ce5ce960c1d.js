3d9e7fc45b03bf9b8291040d8dc913b4aa950564
brh174 <bryant.r.hutchins@gmail.com>
2020-09-08 16:12:01 -0400

    Updates

---
+++ precache-manifest.ac32b76385662877c7956ce5ce960c1d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb8e53ad042cf9d76eb6ce70ed3b48b",
+    "url": "/Library-Startup/index.html"
+  },
+  {
+    "revision": "49dd067160c1ae999470",
+    "url": "/Library-Startup/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "16de6bbf7dfaf083232b",
+    "url": "/Library-Startup/static/css/main.89a44abf.chunk.css"
+  },
+  {
+    "revision": "49dd067160c1ae999470",
+    "url": "/Library-Startup/static/js/2.6a0d7165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Library-Startup/static/js/2.6a0d7165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16de6bbf7dfaf083232b",
+    "url": "/Library-Startup/static/js/main.cecc2f14.chunk.js"
+  },
+  {
+    "revision": "08625d9b9b6cfd3d8aee",
+    "url": "/Library-Startup/static/js/runtime-main.7fd8a1d7.js"
+  },
+  {
+    "revision": "e8579bf52fa2d68d609af378166901fa",
+    "url": "/Library-Startup/static/media/Sketch Gothic School.e8579bf5.ttf"
+  },
+  {
+    "revision": "4a5258e338a2439231cb4ea960d6d615",
+    "url": "/Library-Startup/static/media/The Blacklist.4a5258e3.ttf"
+  },
+  {
+    "revision": "b7f079f65dabd5b03317ca5f9b9678d4",
+    "url": "/Library-Startup/static/media/beachReading.b7f079f6.jpg"
+  },
+  {
+    "revision": "56e574334ebac0062ce31038903a67b5",
+    "url": "/Library-Startup/static/media/books.56e57433.jpg"
+  },
+  {
+    "revision": "b4b7b109c515b3c82921b7a287030d22",
+    "url": "/Library-Startup/static/media/handHolding.b4b7b109.jpg"
+  },
+  {
+    "revision": "e60a5880ca76b60fa87b031253dfa6b6",
+    "url": "/Library-Startup/static/media/yellowedBook.e60a5880.jpg"
+  }
+]);
