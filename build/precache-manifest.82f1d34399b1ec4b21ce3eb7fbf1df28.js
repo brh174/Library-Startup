e76ace0b0ccaad45e10a28df644d193840ced9f6
brh174 <bryant.r.hutchins@gmail.com>
2020-09-10 13:41:33 -0400

    fixing size

---
+++ build/precache-manifest.82f1d34399b1ec4b21ce3eb7fbf1df28.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15278380279b1b3186a9af3030c5f5ef",
+    "url": "/Library-Startup/index.html"
+  },
+  {
+    "revision": "49dd067160c1ae999470",
+    "url": "/Library-Startup/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5007bf44f699920f461b",
+    "url": "/Library-Startup/static/css/main.39fd7f96.chunk.css"
+  },
+  {
+    "revision": "49dd067160c1ae999470",
+    "url": "/Library-Startup/static/js/2.6a0d7165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Library-Startup/static/js/2.6a0d7165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5007bf44f699920f461b",
+    "url": "/Library-Startup/static/js/main.c6dc029b.chunk.js"
+  },
+  {
+    "revision": "08625d9b9b6cfd3d8aee",
+    "url": "/Library-Startup/static/js/runtime-main.7fd8a1d7.js"
+  },
+  {
+    "revision": "e8579bf52fa2d68d609af378166901fa",
+    "url": "/Library-Startup/static/media/Sketch Gothic School.e8579bf5.ttf"
+  },
+  {
+    "revision": "4a5258e338a2439231cb4ea960d6d615",
+    "url": "/Library-Startup/static/media/The Blacklist.4a5258e3.ttf"
+  },
+  {
+    "revision": "b7f079f65dabd5b03317ca5f9b9678d4",
+    "url": "/Library-Startup/static/media/beachReading.b7f079f6.jpg"
+  },
+  {
+    "revision": "56e574334ebac0062ce31038903a67b5",
+    "url": "/Library-Startup/static/media/books.56e57433.jpg"
+  },
+  {
+    "revision": "b4b7b109c515b3c82921b7a287030d22",
+    "url": "/Library-Startup/static/media/handHolding.b4b7b109.jpg"
+  },
+  {
+    "revision": "e60a5880ca76b60fa87b031253dfa6b6",
+    "url": "/Library-Startup/static/media/yellowedBook.e60a5880.jpg"
+  }
+]);
--- src/Components/Tabs/tabs.js
+++ src/Components/Tabs/tabs.js
@@ -10,15 +10,15 @@ const Tabs = ({selectedTab, handleTabChange}) => {
   	<section className='tabs'>
 	  	<div className='container'>
 	  		<div id='tab-1' className={(selectedTab ==='tab-1') ? 'tab-item tab-border' : 'tab-item'} onClick={() => handleTabChange('tab-1')}>
-	  			<FontAwesomeIcon icon={faUsers} size='4x' />
+	  			<FontAwesomeIcon icon={faUsers} size='3x' />
 	  			<p className='hide-sm'>Crowd sourced</p>
 	  		</div>
 	  		<div id='tab-2' className={(selectedTab ==='tab-2') ? 'tab-item tab-border' : 'tab-item'} onClick={() => handleTabChange('tab-2')}>
-	  			<FontAwesomeIcon icon={faTabletAlt} size='4x'  />
+	  			<FontAwesomeIcon icon={faTabletAlt} size='3x'  />
 	  			<p className='hide-sm'>Read anywhere</p>
 	  		</div>
 	  		<div id='tab-3' className={(selectedTab ==='tab-3') ? 'tab-item tab-border' : 'tab-item'} onClick={() => handleTabChange('tab-3')}>
-	  			<FontAwesomeIcon icon={faHandshake} size='4x' />
+	  			<FontAwesomeIcon icon={faHandshake} size='3x' />
 	  			<p className='hide-sm'>Completely free</p>
 	  		</div>
 	  	</div>
